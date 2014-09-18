d6b1d375a6b33e7e058f3d0e87b8522cc4dea4a8
S. Andrew Sheppard <andrew@wq.io>
2014-09-18 15:17:22 -0500

    fix PIL and magic imports (from f1f62ca)

--- views.py
+++ views.py
@@ -1,10 +1,10 @@
 from rest_framework.response import Response
-from wq.db.rest.views import View
+from wq.db.rest.views import SimpleView
 from wq.db.rest.renderers import binary_renderer
 from .util import generate_image
 
 
-class GenerateView(View):
+class GenerateView(SimpleView):
     renderer_classes = [binary_renderer('image/jpeg')]
 
     def get(self, request, size, image):
